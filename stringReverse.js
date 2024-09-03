506288e7983976cc3a8ee3d35b5f1b7ad4054870
mh neloy <mhneloy708@gmail.com>
2024-09-03 21:21:33 +0600

    second method to reverse the string

--- stringReverse.js
+++ stringReverse.js
@@ -5,3 +5,9 @@ for (var i = sentence.length-1; i >= 0; i--){
     reverse1 += sentence[i];
 }
 console.log(reverse1);
+// reverse the sentence method 2
+let reverse2 = "";
+for (var i of sentence) {
+    reverse2 = i + reverse2;
+}
+console.log(reverse2);
