ff15e348c82efce4c45bd7e25d10260236a7f51e
mh neloy <mhneloy708@gmail.com>
2024-09-03 23:50:17 +0600

    objects

---
+++ object.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "neloy",
+    age: 24,
+    gender: "male",
+    department: "cse",
+    extracurriculam: ["sleeping","playing cricket","foodball","chess","watching movie"]
+}
+// accesssing the value from the objects using older method.
+console.log(person.name);
+// updating the object property value
+person.age = 25;
+console.log(person.age);
