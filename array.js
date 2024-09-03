2614fb272a4526c83029c19f786bd83898b7d6c0
mh neloy <mhneloy708@gmail.com>
2024-09-04 01:28:28 +0600

    sorting number array in asc & dsc order in js

--- array.js
+++ array.js
@@ -1,7 +1,22 @@
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const rev_number = [];
-// inset the number array value in rev_number array using unshift for reversing the number array using for of loop
+// insret the number array value in rev_number array using unshift for reversing the number array using for of loop
 for (let num of number) {
-    rev_number.unshift(num);
+  rev_number.unshift(num);
 }
 console.log(rev_number);
+
+// sorting the number using array
+const numbers = [3, 49, 91, 8, 2, 0, 1];
+
+// ascending order
+const number_asc = numbers.sort(function (a, b) {
+  return a - b;
+});
+console.log(numbers_asc);
+
+// descending order
+const number_dsc = [...numbers].sort(function (a, b) {
+  return b - a;
+});
+console.log(number_dsc);
