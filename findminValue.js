36fec101de83f44b185ff472741528820cdccdb3
mh neloy <mhneloy708@gmail.com>
2024-09-07 16:52:52 +0600

    create a Math object to find minimun & maximum value of two numbers

---
+++ findminValue.js
@@ -0,0 +1,10 @@
+const Math = {
+    min: (num1, num2) => {
+        return (num1 > num2)? num2 : num1;
+    },
+    max: (num1, num2) => {
+        return (num1 > num2) ? num1 : num2;
+    }
+}
+console.log(`Minimum value is ${Math.min(20, -20)}`);
+console.log(`Maxmimum value is ${Math.max(20, -20)}`);
