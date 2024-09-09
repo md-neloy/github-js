d8d732cdbeb79d3b9b53d22245ec6276442e71d7
mh neloy <mhneloy708@gmail.com>
2024-09-09 22:50:30 +0600

    make password challange

--- password.js
+++ password.js
@@ -4,7 +4,7 @@ const koli = {
     siteName:"google"
 }
 function password(obj) {
-    if (obj.birthYear.toString().length != 4) return "invalide";
+    if (obj.birthYear.toString().length != 4 || obj.birthYear === undefined || obj.siteName === undefined || obj.siteName === undefined) return "invalide";
     let passwordString = obj.siteName[0].toUpperCase() +obj.siteName.substring(1) + '#' + obj.name + '@' + obj.birthYear;
     return passwordString;
 }
